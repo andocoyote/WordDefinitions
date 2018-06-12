3dce316c915fa09f334c8a24ec4659657e0ec7d6
Andrew Mikesell <amikesel@ntdev.microsoft.com>
2018-06-12 12:20:02 -0700

    Adding use of difflib::get_close_matches to find possible options for misspelled words

--- WordDefinitions.py
+++ WordDefinitions.py
@@ -1,6 +1,7 @@
 import json
 
 from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 
 def LoadDictionary():
@@ -10,18 +11,42 @@ def LoadDictionary():
 
     return data
 
-
-def GetDefinition(word, data):
+# Definitions will be returned as a dictionary with 'word' as the key to a list of definitions
+def GetDefinitions(word, data):
     
-    definition = None
+    definitions = {}
 
     # Find the definition for the word, else tell the user the word was not found
     if(word in data):
-        definition = data[word]
+        definitions[word] = data[word]
     else:
-        definition = list(["'{0}' was not found in the dictionary".format(word)])
+        # We didn't find the word. Get a list of close matches and we'll display all of them
+        # get_close_matches uses SequenceMatcher to determine a similarity percentage
+        close_matches = get_close_matches(word, data.keys(), len(data), cutoff=0.85)
 
-    return definition
+        # If no close matches were found, inform the user
+        if(len(close_matches) == 0):
+            definitions["Word not found"] = list(["'{0}' was not found in the dictionary".format(word)])
+        else:
+            for match in close_matches:
+                definitions[match] = data[match]
+
+    return definitions
+
+# Display the definition:
+# Print the word followed by a number list of definitions
+def DisplayDefinitions(definitions):
+
+    for key,value in definitions.items():
+
+        print("{0}: ".format(key))
+
+        for i in range(len(value)):
+        # If there are multiple definitions, precede each one with a definition number
+            if(len(value) > 1):
+                print("{0}. ".format(i+1), end='')
+
+            print("{0}".format(value[i]))
 
 
 if __name__ == "__main__":
@@ -32,13 +57,7 @@ if __name__ == "__main__":
 
     # Obtain the word from the user
     word = input('Enter a word to get the definition: ').lower()
-    definition = GetDefinition(word, data)
+    definitions = GetDefinitions(word, data)
 
-    # Display the definition
-    for i in range(len(definition)):
-
-        # If there are multiple definitions, precede each one with a definition number
-        if(len(definition) > 1):
-            print("{0}. ".format(i+1), end='')
-
-        print("{0}".format(definition[i]))
+    # Display the definitions
+    DisplayDefinitions(definitions)
